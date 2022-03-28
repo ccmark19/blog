459d801d3b8361221abb9a249c8dd05d1046d4b0
ccmark19 <cc.mark19@gmail.com>
2022-03-27 22:56:38 -0400

    page init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./profile.scss";
+import ske from "./img/ske.png";
+import ski from "./img/ski.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Comp-content">
+        <img
+          className="Data-img-ske"
+          src={ske}
+          alt="art"
+          style={{
+            height: "500px",
+            width: "300px",
+            // figure out your image aspect ratio
+            aspectRatio: 135 / 76,
+          }}
+        />
+        {/* <img
+          className="Data-img-ski"
+          src={ski}
+          alt="art"
+          style={{
+            height: "311px",
+            width: "382px",
+            // figure out your image aspect ratio
+            aspectRatio: 135 / 76,
+          }}
+        /> */}
+        <div className="Comp-inner-content">
+          {/* <h1 className="Data-title m-0">
+            <span>Welcome </span>
+            <span>I'm Hanxiang Li</span>
+          </h1> */}
+          <p className="Data-desc m-0">
+            <span className="primary">
+              <span className="Data-keyword">Hanxiang</span> (Hanxiang Li to his
+              parents) is a Software Developer 🤓 currently based in Toronto, CA
+              🇨🇦. Over the past few years, he has been continuously learning and
+              applying what he has learned to his works 🛠.
+            </span>
+            <span className="secondary">
+              He is passionate about{" "}
+              <span className="Data-keyword">Front End Dev</span>,{" "}
+              <span className="Data-keyword">Performance</span> and{" "}
+              <span className="Data-keyword">User Experience</span>.
+            </span>
+          </p>
+          <div className="Comp-detail"></div>
+        </div>
+      </div>
     </div>
   );
 }
